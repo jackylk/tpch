48e9db23334f913cb90eb913d6a9bb8cff7897d8
Jacky Li <jacky.likun@qq.com>
2018-09-25 21:28:53 +0800

    modify script

--- cluster_script/run_carbon_query.sh
+++ cluster_script/run_carbon_query.sh
@@ -2,3 +2,4 @@
 $SPARK_HOME/bin/beeline -u jdbc:hive2://carbon1:10000 -f ./carbon_queries.sql  2>&1 | tee test/carbon.log
 
 grep seconds test/carbon.log | sed '1,9d' | sed '15d' | sed '16d' | awk '{print substr($4, 2)}' > report/carbon-time.txt
+grep seconds test/carbon.log | sed '1,9d' | sed '15d' | sed '16d' | awk '{print $1}' > report/carbon-result.txt
